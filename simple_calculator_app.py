bb78a4566628fce586ff533c125263ae094db8cc
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 14:30:56 +0800

    added a method to get user inputs using input()

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -2,3 +2,9 @@ class SimpleCalculator:
     def __init__(self, first_input, second_input):
         self.first_input = first_input
         self.second_input = second_input
+
+    def get_input(self):
+        self.first_input = input("Enter your first number: ")
+        self.second_input = input("Enter your second number: ")
+
+
